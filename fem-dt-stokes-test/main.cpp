8a041ed0684fb33088815c9815d7fbe160fdaf90
vitek <refactorman@gmail.com>
2013-04-19 17:14:44 +1000

    закончил редактировать, надо литературы добавить

--- fem-dt-stokes-test/main.cpp
+++ fem-dt-stokes-test/main.cpp
@@ -7,6 +7,10 @@
 using namespace dolfin;
 
 int main(int argc, char *argv[]){
+    UnitSquare _mesh(40, 40);
+    plot(_mesh);
+    interactive();
+    return 0;
     Settings settings(argc, argv);
     info("Using parameters: " + settings.toString());
     SolverFactory factory(settings);
